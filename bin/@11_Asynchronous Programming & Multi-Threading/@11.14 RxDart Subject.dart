ef97226c40fcd2c547204dce9913ba62d3e3cf29
Myo Min Latt <myominlatt.sbs@gmail.com>
2022-11-15 22:47:59 +0630

    @29.1 singleton

--- bin/@11_Asynchronous Programming & Multi-Threading/@11.14 RxDart Subject.dart
+++ bin/@11_Asynchronous Programming & Multi-Threading/@11.14 RxDart Subject.dart
@@ -14,28 +14,49 @@ import 'package:rxdart/rxdart.dart';
 //   replaySubject.add('event2');
 // }
 
+// void main(){
+//   BehaviorSubject behaviorSubject = BehaviorSubject();
+//   behaviorSubject.add('event1');
+//   behaviorSubject.add('event2');
+//   behaviorSubject.listen((value) {print('Listener1.$value');}); // call first value before
+//   behaviorSubject.add('event3');
+//   behaviorSubject.listen((value) {print('Listener2.$value');}); // call first value before
+//   behaviorSubject.add('event4');
+// }
 
-/**
-void main(){
-  BehaviorSubject behaviorSubject = BehaviorSubject();
-  behaviorSubject.add('event1');
-  behaviorSubject.add('event2');
-  behaviorSubject.listen((value) {print('Listener1.$value');}); // call first value before
-  behaviorSubject.add('event3');
-  behaviorSubject.listen((value) {print('Listener2.$value');}); // call first value before
-  behaviorSubject.add('event4');
-}
- */
+// void main() {
+//   PublishSubject publishSubject = PublishSubject();
+//   publishSubject.add('event1');
+//   publishSubject.add('event2');
+//   publishSubject.listen((value) {
+//     print('Listener1.$value');
+//   });
+//   publishSubject.add('event3');
+//   publishSubject.listen((value) {
+//     print('Listener2.$value');
+//   });
+//   publishSubject.add('event4');
+// }
 
-
-
-
-void main(){
+void main() {
   PublishSubject publishSubject = PublishSubject();
-  publishSubject.add('event1');
-  publishSubject.add('event2');
-  publishSubject.listen((value) {print('Listener1.$value');});
-  publishSubject.add('event3');
-  publishSubject.listen((value) {print('Listener2.$value');});
-  publishSubject.add('event4');
+  publishSubject.add(1);
+  publishSubject.add(2);
+  List<int> intList = [];
+  publishSubject.listen(
+    (value) {
+      print('Listener1.$value');
+      intList.add(value);
+      print(intList);
+    },
+    onDone: () => print('intList'),
+    // onError: () => print('on Error'),
+    // cancelOnError: false,
+  );
+  // print(intList);
+  publishSubject.add(3);
+  publishSubject.listen((value) {
+    print('Listener2.$value');
+  });
+  publishSubject.add(4);
 }
--- bin/@11_Asynchronous Programming & Multi-Threading/@11.15 Isolate.dart
+++ bin/@11_Asynchronous Programming & Multi-Threading/@11.15 Isolate.dart
@@ -2,23 +2,23 @@
 // mutli thread// own thread// one parameter in method
 
 import 'dart:isolate';
-void main(){
-  Isolate.spawn(work1, 6);
-  work2(5);
+
+void main() {
+  Isolate.spawn(work1, 2);
+  work2(3);
   // print('Main function => ${Isolate.current.debugName} Isolate');
 }
-void work1(int end){
-  print('Work1 =>  ${Isolate.current.debugName} Isolate');
-  for(int i=1; i<=end; i++){
+
+void work1(int end) {
+  // print('Work1 =>  ${Isolate.current.debugName} Isolate');
+  for (int i = 1; i <= end; i++) {
     print('Work1 $i');
   }
 }
-void work2(int end){
-  print('Work2 => ${Isolate.current.debugName} Isolate');
-  for(int i=1; i<=end; i++){
+
+void work2(int end) {
+  // print('Work2 => ${Isolate.current.debugName} Isolate');
+  for (int i = 1; i <= end; i++) {
     print('Work2 $i');
   }
 }
-
-
-
